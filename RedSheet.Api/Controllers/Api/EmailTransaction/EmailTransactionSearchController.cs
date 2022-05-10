2c27733e70edbfd7c03a99a1eae1361775e63652
Patel Uttam <uttam.patel@radixweb.com>
2022-05-10 13:31:00 +0530

    Uttam : WebApp : implement search and filter in messaging module

--- RedSheet.Api/Controllers/Api/EmailTransaction/EmailTransactionSearchController.cs
+++ RedSheet.Api/Controllers/Api/EmailTransaction/EmailTransactionSearchController.cs
@@ -6,23 +6,34 @@ using Rx.Core.Data;
 using Rx.Core.Security;
 using RedSheet.BoundedContext.SqlContext;
 using RedSheet.ViewModels.Models;
+using RedSheet.DbEntities.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace RedSheet.Api.Controllers
 {
     [Route("api/EmailTransaction/search")]
     public class EmailTransactionSearchController : BaseController
     {
+        private int query;
+
         private IDbContextManager<MainSqlDbContext> DbContextManager { get; set; }
         public EmailTransactionSearchController(IDbContextManager<MainSqlDbContext> dbContextManager)
         {
             DbContextManager = dbContextManager;
         }
         [HttpPost]
-        public async Task<IActionResult> Post(StoreProcSearchModel storeProcSearch)
-        {
-            var spParameters = new object[1];
+        public async Task<IActionResult> Post([FromBody] StoreProcSearchModel searchFilter)
+            {
+
+            var query = JObject.Parse(searchFilter.Query);
+
+            var spParameters = new object[4];
             spParameters[0] = new SqlParameter() { ParameterName = "UserId", Value = UserClaim.UserId };
-            var storeProcSearchResult = await DbContextManager.SqlQueryAsync<StoreProcSearchViewModel>("EXEC dbo.spEmailTransactions @UserId", spParameters); // change ' dbo.spEmailTransaction ' to ' dbo.spEmailTransactions '
+            spParameters[1] = new SqlParameter() { ParameterName = "SearchBy", Value = query["Query"][0]["searchValue"].ToString() };
+            spParameters[2] = new SqlParameter() { ParameterName = "OrderByDate", Value = query["Query"][0]["dateOrder"].ToString() };
+            spParameters[3] = new SqlParameter() { ParameterName = "CategoryBy", Value = query["Query"][0]["emailCategory"].ToString() };
+            var storeProcSearchResult = await DbContextManager.SqlQueryAsync<StoreProcSearchViewModel>("EXEC dbo.spEmailTransactions @UserId , @SearchBy , @OrderByDate , @CategoryBy", spParameters); // change ' dbo.spEmailTransaction ' to ' dbo.spEmailTransactions '
             return Ok(storeProcSearchResult.SingleOrDefault()?.Result);
         }
 
--- redsheet-portal/src/app/components/email-transaction/list/email-transaction-list.component.ts
+++ redsheet-portal/src/app/components/email-transaction/list/email-transaction-list.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, OnDestroy,  Input,ComponentFactoryResolver} from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, ComponentFactoryResolver } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs/Rx';
-import {RxToast, RxDialog, DialogClick } from '@rx/view';
+import { RxToast, RxDialog, DialogClick } from '@rx/view';
 
-import {  EmailTransaction} from 'app/database-models/email-transaction';
+import { EmailTransaction } from 'app/database-models/email-transaction';
 import { EmailTransactionService } from 'app/components/email-transaction/email-transaction.service';
 import { EmailTransactionDomain } from 'app/components/email-transaction/domain/email-transaction.domain';
 
@@ -11,47 +11,82 @@ import { EmailTransactionDomain } from 'app/components/email-transaction/domain/
 
 @Component({
   templateUrl: './email-transaction-list.component.html',
-  })
+})
 export class EmailTransactionListComponent extends EmailTransactionDomain implements OnInit, OnDestroy {
   showComponent: boolean = false;
   emailTransaction: EmailTransaction[];
-  listSubscription: Subscription; 
+  listSubscription: Subscription;
 
-  filterOption:any[]=["SENT","RECEIVED"];
-  dateOrder:string = "ASCENDING";
-  liReapet:any[]=[1,2,3,4,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+  searchValue: string = "";
+  emailCategory: any[] = ["SENT", "RECIEVED"];
+  emailCategoryValue: string = "";
+  dateOrder: string = "DESCENDING";
+  searchFilter = { searchValue: this.searchValue, dateOrder: this.dateOrder, emailCategory: this.emailCategoryValue }
 
   constructor(
-    private emailTransactionService: EmailTransactionService,    
+    private emailTransactionService: EmailTransactionService,
     private dialog: RxDialog,
     private router: Router,
-  ) { super();}
+  ) { super(); }
 
   ngOnInit(): void {
-    if (this.listSubscription)
-        this.listSubscription.unsubscribe();
-    this.listSubscription = this.emailTransactionService.search({Query: []}).subscribe(emailTransaction => {
-        this.emailTransaction = emailTransaction.result;
-        this.showComponent = true;
-    });
+    this.EmailTransactions();
   }
   // date order toggle 
-  DateOrder()
-  {
-    if(this.dateOrder == "ASCENDING")
-    {
+  DateOrderEmailTransaction() {
+    if (this.dateOrder == "ASCENDING") {
       this.dateOrder = "DESCENDING";
+      this.searchFilter.dateOrder = this.dateOrder;
     }
-    else
-    {
+    else {
       this.dateOrder = "ASCENDING";
+      this.searchFilter.dateOrder = this.dateOrder;
     }
+    this.EmailTransactions();
   }
 
- ngOnDestroy(): void {
-      if (this.listSubscription)
-          this.listSubscription.unsubscribe();
-      super.destroy();
+  EmailTransactions() {
+    console.log(this.emailTransaction);
+
+    if (this.listSubscription) {
+      this.listSubscription.unsubscribe();
+    }
+
+    this.listSubscription = this.emailTransactionService.search({ Query: [this.searchFilter] }).subscribe(emailTransaction => {
+      this.emailTransaction = null;
+      this.emailTransaction = (!emailTransaction.result) ? [] : emailTransaction.result;
+      console.log("added", this.emailTransaction);
+
+      this.showComponent = true;
+    });
+  }
+
+  SearchEmailTransaction() {
+    this.searchFilter.searchValue = this.searchValue;
+    this.EmailTransactions();
+  }
+
+  CategoryEmailTransaction()
+  {
+    this.searchFilter.emailCategory = this.emailCategoryValue;
+    this.EmailTransactions();
+  }
+  ResetEmailTransactionFilter() {
+
+    this.searchValue = "";
+    this.dateOrder = "DESCENDING";
+    this.emailCategoryValue = "";
+    this.searchFilter.searchValue = "";
+    this.searchFilter.dateOrder = "DESCENDING";
+    this.searchFilter.emailCategory = "";
+
+    this.EmailTransactions();
+  }
+
+  ngOnDestroy(): void {
+    if (this.listSubscription)
+      this.listSubscription.unsubscribe();
+    super.destroy();
   }
 }
 
